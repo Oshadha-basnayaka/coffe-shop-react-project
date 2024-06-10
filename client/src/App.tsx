608086359fa627c2eb3ff9f0c98dd0f8497eae1b
Oshadha Basnayaka <oshadhabasnayaka69@gmail.com>
2024-06-10 20:58:24 +0900

    added admin panel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {DefaultLayout} from "./view/commen/DefaultLayout/DefaultLayout";
 import {Login} from "./view/page/Login/Login";
 import {SingUp} from "./view/page/Login/SingUp";
+import AdminLayout from './view/commen/DefaultLayout/AdminLayout';
 
 function App() {
     return ( <BrowserRouter>
@@ -16,6 +17,12 @@ function App() {
                 <Route path="/*"
                        Component={DefaultLayout}>
                 </Route>
+
+                <Route path='/admin'
+                Component={AdminLayout}>
+                
+                </Route>
+
                 <Route path="/login"
                        Component={Login}>
                 </Route>
